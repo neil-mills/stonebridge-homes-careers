21c2bc9ac43c5601f88f4f07261cb9959f85cee1
Neil Mills <mail@neil-mills.co.uk>
2021-12-22 10:00:23 +0000

    add animation to nav

--- gatsby/src/components/Nav.tsx
+++ gatsby/src/components/Nav.tsx
@@ -1,14 +1,14 @@
-import React, { FC } from 'react'
+import React, { FC, MouseEvent, useState, useEffect, useRef } from 'react'
 import { Link } from 'gatsby'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { FontBold } from '../assets/styles/Typography'
-import { WhiteHover } from '../assets/styles/Utils'
 import MenuButton from './MenuButton'
 import { NavigationLink } from '../types/navigation'
 
-const NavStyles = styled.nav`
+const NavStyles = styled.nav<{ left: number; width: number }>`
   display: none;
   height: 100%;
+  position: relative;
   @media screen and (min-width: 768px) {
     display: block;
   }
@@ -25,7 +25,7 @@ const NavStyles = styled.nav`
     align-items: center;
     position: relative;
     &[data-active='true'] {
-      &:after {
+      /* &:after {
         content: '';
         display: block;
         position: absolute;
@@ -34,39 +34,100 @@ const NavStyles = styled.nav`
         width: 100%;
         height: 2px;
         background-color: var(--gold);
-      }
+      } */
     }
   }
   a {
+    color: var(--white);
     ${FontBold}
     font-size: var(--font-small);
     text-transform: uppercase;
     transition: color 200ms ease;
-    ${WhiteHover}
+    &:hover,
+    &:active,
+    &:focus,
     &[aria-current] {
       color: var(--gold);
     }
+  }
+  span {
+    display: block;
+    height: 3px;
+    ${({ left, width }) => css`
+      left: ${left}px;
+      width: ${width}px;
+    `}
+    background-color: var(--gold);
+    position: absolute;
+    bottom: 0;
+    transition: left 200ms ease, width 200ms ease;
   }
 `
 
 interface NavProps {
   options: NavigationLink[]
 }
+
 const Nav: FC<NavProps> = ({ options }) => {
+  const [activeLeft, setActiveLeft] = useState(0)
+  const [activeWidth, setActiveWidth] = useState(0)
+  const [hoverLeft, setHoverLeft] = useState(0)
+  const [hoverWidth, setHoverWidth] = useState(0)
+  const liRefs = useRef<HTMLLIElement[]>([])
+  let delay: ReturnType<typeof setTimeout> | null = null
+
+  const handleHover = (e: MouseEvent) => {
+    if (delay) clearTimeout(delay)
+    if ((e.target as HTMLAnchorElement).parentElement) {
+      const parent = (e.target as HTMLAnchorElement).parentElement
+      if (parent) {
+        const width: number = parent.clientWidth
+        const left = parent.offsetLeft
+        setHoverLeft(left)
+        setHoverWidth(width)
+      }
+    }
+  }
+
+  const handleLeave = () => {
+    delay = setTimeout(() => {
+      setHoverWidth(activeWidth)
+      setHoverLeft(activeLeft)
+    }, 500)
+  }
+
+  useEffect(() => {
+    liRefs.current.forEach(li => {
+      if (li.getAttribute('data-active') === 'true') {
+        setActiveLeft(li.offsetLeft)
+        setActiveWidth(li.clientWidth)
+        setHoverLeft(li.offsetLeft)
+        setHoverWidth(li.clientWidth)
+      }
+    })
+  }, [])
+
   return (
     <>
-      <MenuButton />
-      <NavStyles>
+      <NavStyles left={hoverLeft} width={hoverWidth}>
         <ul>
-          {options.map((option: NavigationLink) => (
+          {options.map((option: NavigationLink, i: number) => (
             <li
               key={option.slug.current}
               data-active={location.pathname === `/${option.slug.current}`}
+              ref={(element: HTMLLIElement) => (liRefs.current[i] = element)}
             >
-              <Link to={`/${option.slug.current}`}>{option.title}</Link>
+              <Link
+                onMouseOver={handleHover}
+                onMouseLeave={handleLeave}
+                to={`/${option.slug.current}`}
+              >
+                {option.title}
+              </Link>
             </li>
           ))}
         </ul>
+        <span></span>
       </NavStyles>
     </>
   )
