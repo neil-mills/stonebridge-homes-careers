d7d86d43142b7304e8a9f7ad85cd3970375b1d37
Neil Mills <mail@neil-mills.co.uk>
2022-01-21 10:57:25 +0000

    Update breakpoint

--- gatsby/src/components/HomeBanner.tsx
+++ gatsby/src/components/HomeBanner.tsx
@@ -133,7 +133,7 @@ const StyledHomeBanner = styled(HomeBanner)`
     height: calc(60vw - 7.6rem);
     max-height: 60rem;
   }
-  @media screen and (min-width: 1600px) {
+  @media screen and (min-width: 1400px) {
     background-size: 1390px auto;
     background-position: right bottom;
     background-repeat: no-repeat;
