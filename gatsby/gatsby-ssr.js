7e38fc05bc656415db9f9899a82d573031f047f1
Neil Mills <mail@neil-mills.co.uk>
2022-03-07 16:03:10 +0000

    SSR Update

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -1,11 +1,6 @@
-const React = require('react')
-const Layout = require('./src/components/Layout')
-const { AppContextProvider } = require('./src/context/AppContext')
+import React from 'react'
+import Layout from './src/components/Layout'
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
-}
-
-export const wrapRootElement = ({ element }) => {
-  return <AppContextProvider>{element}</AppContextProvider>
 }
